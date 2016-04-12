2a4c836a417ed2d7d02edcbc98070b67762aca8b
duanlei <455483293@qq.com>
2016-04-12 17:07:40 +0800

    Initial Commit

---
+++ GoodCompany/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodCompany
+//
+//  Created by 段磊 on 16/4/12.
+//  Copyright © 2016年 段磊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
